3cb0a872a2cb82c2dc5f26b72b1a47643d6e55d2
AkshayAntonyMT <akshayantonymt2004@gmail.com>
2025-09-11 08:46:56 +0530

    Changed indexing to integer type

--- UART_TEST0.srcs/sources_1/new/txx.v
+++ UART_TEST0.srcs/sources_1/new/txx.v
@@ -3,7 +3,7 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 08/06/2025 11:14:00 PM
+// Create Date: 08/07/2025 10:02:36 PM
 // Design Name: 
 // Module Name: txx
 // Project Name: 
@@ -22,9 +22,9 @@
 
 module txx(
 input clk,
-input start_tx,
 input [7:0] data_in,
-output reg tx
+//output reg next = 1,
+output reg tx  
 );
  
  //parameters for setting baud rate
@@ -37,28 +37,28 @@ parameter DATA  = 2;
 parameter STOP  = 3;
 
 //registers
-reg [1:0] state      = IDLE;
-reg [15:0] clk_count = 0;
-reg [2:0] bit_index  = 0;
-reg [7:0] tx_data    = 8'd0;
+reg [1:0] state    = IDLE;
+reg [30:0] clk_count = 0;
+integer bit_index  = 0;
 
-//always block
+
 always@(posedge clk)
 begin
     case(state)
     IDLE:begin
         tx <= 1;
+        if (clk_count < 5)
+            clk_count <= clk_count +1;
+        else begin
         clk_count <= 0;
         bit_index <= 0;
-        if (start_tx)                       //ith aavshyam illa.ith illelum data transfer aavilla
-            begin                           //ee start or switch akki kodkkand puthiya value read avumbo transmitt enable aavanam.
-            tx_data <= data_in;             
-            state <= START;
-            end
+        //next <= 0;                             
+        state <= START;
+        end
         end
     START:begin
           tx <= 0;
-          if (clk_count < bit_period - 1)
+          if (clk_count < bit_period)
             clk_count <= clk_count +1;
           else 
             begin
@@ -67,27 +67,28 @@ begin
             end
           end
     DATA:begin
-         tx <= tx_data[bit_index];
-         if (clk_count < bit_period - 1)
+         tx <= data_in[bit_index];
+         if (clk_count < bit_period)
             clk_count <= clk_count +1;
          else 
             begin
             clk_count <= 0;
             if (bit_index < 7)
-                bit_index <= bit_index + 1;
+            bit_index <= bit_index + 1;
             else
                 state <= STOP;
             end
          end
      STOP:begin
           tx <= 1;
-          if (clk_count < bit_period - 1)
+          if (clk_count < bit_period)
             clk_count <= clk_count +1;
           else 
             begin
             clk_count<= 0;
             bit_index <= 0;
-            state <= IDLE;          //oru byte transmit cheyth kazhinj maathram read adress increment cheyth adtha value read aakkanam.
+            //next <= 1;
+            state <= IDLE;                                
             end
           end
     endcase
